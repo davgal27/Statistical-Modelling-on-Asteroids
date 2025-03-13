544d789514b1a993cf36d0fc52a60d8da16dde66
giuliadant <giulia.dant2003@gmail.com>
2025-03-13 12:05:15 +0100

    Update kurtosis_skewness_analysis.R

--- kurtosis_skewness_analysis.R
+++ kurtosis_skewness_analysis.R
@@ -3,14 +3,14 @@ library(e1071)
 
 # Compute and print skewness
 print("Skewness Results:")
-print(skewness(meteorite_dataset$mass, na.rm = TRUE))
-print(skewness(meteorite_dataset$year, na.rm = TRUE))
-print(skewness(meteorite_dataset$reclat, na.rm = TRUE))
-print(skewness(meteorite_dataset$reclong, na.rm = TRUE))
+print(skewness(meteorite_dataset$mass))
+print(skewness(meteorite_dataset$year))
+print(skewness(meteorite_dataset$reclat))
+print(skewness(meteorite_dataset$reclong))
 
 # Compute and print kurtosis
 print("Kurtosis Results:")
-print(kurtosis(meteorite_dataset$mass, na.rm = TRUE))
-print(kurtosis(meteorite_dataset$year, na.rm = TRUE))
-print(kurtosis(meteorite_dataset$reclat, na.rm = TRUE))
-print(kurtosis(meteorite_dataset$reclong, na.rm = TRUE))
+print(kurtosis(meteorite_dataset$mass))
+print(kurtosis(meteorite_dataset$year))
+print(kurtosis(meteorite_dataset$reclat))
+print(kurtosis(meteorite_dataset$reclong))
